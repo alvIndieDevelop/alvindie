979b0485171fbbe306d0c3a90a0690d9dce04a7c
alvindie <alvindiedevelop@gmail.com>
2018-11-11 07:51:09 -0500

    haciendo el landingPage

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { Switch, Route } from "react-router-dom";
 // pages
 import LandingPage from "./pages/landingPage/LandingPage";
 
+const baseURL = process.env.PUBLIC_URL;
+
 const App = () => (
   <Switch>
-    <Route exact path="/" component={LandingPage} />
+    <Route exact path={baseURL + "/"} component={LandingPage} />
   </Switch>
 );
 
